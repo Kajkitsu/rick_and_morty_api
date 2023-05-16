54a795d4b268a64efe2312ebaff92f4bd35d295a
nwaszkowiak <norbert.waszkowiak@gmail.com>
2023-05-16 21:23:51 +0200

    update CharacterDTO and RickAndMortyFetcher

--- src/main/java/org/example/CharacterDTO.java
+++ src/main/java/org/example/CharacterDTO.java
@@ -8,11 +8,9 @@ import java.util.List;
 public record CharacterDTO(
         Integer id,
         String name,
-        String status,
         String species,
         String type,
         String gender,
-        String image,
         List<String> episode,
         String url
 ) {
--- src/main/java/org/example/RickAndMortyFetcher.java
+++ src/main/java/org/example/RickAndMortyFetcher.java
@@ -1,50 +1,51 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Collections;
+import java.util.List;
 
 public class RickAndMortyFetcher {
-
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static void main(String[] args) {
-        // Tworzenie instancji HttpClient
-        HttpClient httpClient = HttpClient.newHttpClient();
+        RickAndMortyFetcher fetcher = new RickAndMortyFetcher();
+        fetcher.fetchCharacterByName("Summer")
+                .stream()
+                .map(CharacterDTO::name)
+                .distinct()
+                .forEach(System.out::println);
+    }
 
-        // Adres URL API
-        String apiUrl = "https://rickandmortyapi.com/api/character/?name=pickle";
-
-        // Tworzenie HttpRequest z adresem URL i parametrem
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl))
-                .GET()
-                .build();
-
+    public List<CharacterDTO> fetchCharacterByName(String name) {
+        String apiUrl = "https://rickandmortyapi.com/api/character/?name=" + name;
+        String responseBody = fetch(apiUrl);
         try {
-            // Wysłanie żądania GET i przechwycenie odpowiedzi
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-            // Pobranie statusu odpowiedzi
-            int statusCode = response.statusCode();
-
-            // Pobranie nagłówków odpowiedzi
-            HttpHeaders headers = response.headers();
-
-            // Pobranie ciała odpowiedzi
-            String responseBody = response.body();
-
-            // Mapowanie resposne body do obiektu klasy CharacterResponseDTO
-            CharacterResponseDTO responseDTO = objectMapper.readValue(responseBody, CharacterResponseDTO.class);
-
-            // Wyświetlanie przekonwertowanej odpowiedzi
-            System.out.println("Response body: " + responseDTO.toString());
-        } catch (Exception e) {
+            CharacterResponseDTO responseDTO = OBJECT_MAPPER.readValue(responseBody, CharacterResponseDTO.class);
+            return responseDTO.results();
+        } catch (IOException e) {
             e.printStackTrace();
+            return Collections.emptyList();
         }
     }
+
+    private String fetch(String path) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(path))
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
